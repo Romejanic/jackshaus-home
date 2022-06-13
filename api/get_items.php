85bbb379c51e6e00cf549263d8a467d87095080f
Jack Davenport <jack.davenport5011@gmail.com>
2022-06-13 21:07:16 +1000

    Thing data now comes from database

--- api/get_items.php
+++ api/get_items.php
@@ -1,14 +1,15 @@
 <?php
+    include_once "db.php";
     Header("Content-Type: application/json");
 
     $data = array();
 
-    for($i = 0; $i < 7; $i++) {
+    foreach($db_conn->query("SELECT * FROM Things") as $row) {
         array_push($data, array(
-            "name" => "Test item",
-            "description" => "A fun item I worked on",
-            "image" => "https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg",
-            "url" => "https://jackshaus.ddns.net/sven"
+            "name" => $row["name"],
+            "description" => $row["description"],
+            "image" => $row["image"],
+            "url" => $row["url"]
         ));
     }
 
