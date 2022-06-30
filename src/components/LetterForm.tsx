9c30b828dd0f362bb4bf5a4689f7f6b0357265d9
Jack Davenport <jack.davenport5011@gmail.com>
2022-06-30 15:55:32 +1000

    Changed layout of letter form

--- src/components/LetterForm.tsx
+++ src/components/LetterForm.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Char } from "../data/letter";
 import LetterTile from "./LetterTile";
+import styles from "./LetterForm.module.css";
 
 export default function LetterForm() {
-    const [letter, setLetter] = useState<Char>("A");
+    const [letter, setLetter] = useState<Char>("J");
     const [colour, setColour] = useState("#ffffff");
 
     function onLetterInput(e: React.ChangeEvent<HTMLInputElement>) {
@@ -13,23 +14,27 @@ export default function LetterForm() {
     }
 
     return (
-        <div>
+        <div className={styles.form}>
             <LetterTile data={{ letter, color: colour }} />
-            <div>
-                <label>Letter</label>
-                <input
-                    type="text" maxLength={1}
-                    onChange={onLetterInput}
-                    value={letter}
-                />
-            </div>
-            <div>
-                <label>Colour</label>
-                <input
-                    type="color"
-                    onChange={e => setColour(e.target.value)}
-                    value={colour}
-                />
+            <div className={styles.inputs}>
+                <div>
+                    <label className={styles.label}>Letter</label>
+                    <input
+                        className={styles.input}
+                        type="text" maxLength={1}
+                        onChange={onLetterInput}
+                        value={letter}
+                    />
+                </div>
+                <div>
+                    <label className={styles.label}>Colour</label>
+                    <input
+                        className={styles.input}
+                        type="color"
+                        onChange={e => setColour(e.target.value)}
+                        value={colour}
+                    />
+                </div>
             </div>
         </div>
     );
